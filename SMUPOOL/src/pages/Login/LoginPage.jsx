13ed94ea98c4e238ea409d697825c1a331102722
joyfulond <kimhy030927@gmail.com>
2024-06-07 16:32:15 +0900

    feat:로그인 회원가입 디자인만

--- SMUPOOL/src/pages/Login/LoginPage.jsx
+++ SMUPOOL/src/pages/Login/LoginPage.jsx
@@ -1,5 +1,88 @@
+import styled from "styled-components";
+
+import Logo from "../../assets/images/Logo.webp";
+import { useNavigate } from "react-router-dom";
 const LoginPage = () => {
-  return <div>로그인</div>;
+  const navigate = useNavigate();
+  return (
+    <Container>
+      <img src={Logo} alt="asdf" />
+
+      <Contents>
+        <Title>로그인</Title>
+        <Center>
+          <Input type="text" placeholder="학번" />
+          <Input type="password" placeholder="비밀번호" />
+        </Center>
+        <Error> awef</Error>
+        <Center>
+          <Btn>로그인</Btn>
+          <Btn
+            onClick={() => {
+              navigate("/sign-up");
+            }}
+          >
+            회원가입
+          </Btn>
+        </Center>
+      </Contents>
+    </Container>
+  );
 };
 
 export default LoginPage;
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+const Title = styled.div`
+  font-size: 45px;
+  font-weight: 700;
+`;
+const Error = styled.div`
+  width: 60%;
+  margin: 15px;
+  text-align: center;
+  height: 70px;
+  color: #fa1919;
+  background-color: #e8eaec;
+`;
+const Contents = styled.div`
+  width: 40%;
+  height: 70%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+const Input = styled.input`
+  border: 1px solid #b0b1b3;
+  width: 80%;
+  height: 60px;
+  border-radius: 10px;
+  margin: 15px;
+`;
+const Btn = styled.button`
+  width: 100%;
+  height: 60px;
+  border-radius: 23px;
+  background-color: #186dec;
+  color: white;
+  text-align: center;
+  border: 1px solid #186dec;
+  font-size: 30px;
+  margin-bottom: 5px;
+`;
+const Center = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
--- SMUPOOL/src/pages/Signup/SignupPage.jsx
+++ SMUPOOL/src/pages/Signup/SignupPage.jsx
@@ -1,5 +1,82 @@
-const SignupPage = () => {
-  return <div>회원가입</div>;
-};
+import styled from "styled-components";
 
-export default SignupPage;
+import Logo from "../../assets/images/Logo.webp";
+export default function SignupPage() {
+  return (
+    <Container>
+      <img src={Logo} alt="asdf" />
+
+      <Contents>
+        <Title>
+          <h1>회원가입</h1>
+          <h2>상명대 학생 인증</h2>
+        </Title>
+        <Center>
+          <Input type="text" placeholder="학번" />
+          <Input type="password" placeholder="비밀번호" />
+        </Center>
+        <Center>
+          <Btn>인증</Btn>
+        </Center>
+      </Contents>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+const Title = styled.div`
+  h1 {
+    font-size: 45px;
+    font-weight: 700;
+  }
+  h2 {
+    font-size: 30px;
+    font-weight: 700;
+  }
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const Contents = styled.div`
+  width: 40%;
+  height: 70%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+const Input = styled.input`
+  border: 1px solid #b0b1b3;
+  width: 80%;
+  height: 60px;
+  border-radius: 10px;
+  margin: 15px;
+`;
+const Btn = styled.button`
+  width: 100%;
+  height: 60px;
+  border-radius: 23px;
+  background-color: #186dec;
+  color: white;
+  text-align: center;
+  border: 1px solid #186dec;
+  font-size: 30px;
+  margin-bottom: 5px;
+`;
+const Center = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
