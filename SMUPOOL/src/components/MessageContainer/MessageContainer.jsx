701c5b452b69832bde042cb2865bf422d4a2569c
hijjoy <hijjoy@naver.com>
2024-06-21 16:09:47 +0900

    chore: profile 이미지

--- SMUPOOL/src/components/MessageContainer/MessageContainer.jsx
+++ SMUPOOL/src/components/MessageContainer/MessageContainer.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./MessageContainer.css";
 import { Container } from "@mui/system";
+import Profile from "../../assets/images/profile.webp";
 
 const MessageContainer = ({ messageList, user }) => {
   return (
@@ -18,18 +19,16 @@ const MessageContainer = ({ messageList, user }) => {
               </div>
             ) : (
               <div className="your-message-container">
-                {/* <img
-                  src="/profile.jpeg"
+                <img
+                  src={Profile}
                   className="profile-image"
                   style={
-                    (index === 0
-                      ? { visibility: "visible" }
-                      : messageList[index - 1].user.name === user.name) ||
+                    (index === 0 ? { visibility: "visible" } : messageList[index - 1].user.name === user.name) ||
                     messageList[index - 1].user.name === "system"
                       ? { visibility: "visible" }
                       : { visibility: "hidden" }
                   }
-                /> */}
+                />
                 <div className="your-message">{message.chat}</div>
               </div>
             )}
