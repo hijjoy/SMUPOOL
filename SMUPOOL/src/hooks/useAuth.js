57c84edfe4d84154b81f7b56027d9e9a54c6856c
daindaind <cdin23@naver.com>
2024-06-23 19:13:04 +0900

    Feat: useAuth hook

---
+++ SMUPOOL/src/hooks/useAuth.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+const useAuth = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      setIsAuthenticated(true);
+    }
+  }, []);
+
+  return isAuthenticated;
+};
+
+export default useAuth;
